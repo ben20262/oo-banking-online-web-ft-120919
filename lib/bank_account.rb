f1c977a1b2d67da4ac211aaef528b3520a132892
Benjamin Small <ben20262@aim.com>
2020-01-06 20:12:26 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
+  attr_reader :balance, :status
+  attr_accessor :name
+
+  def initialize (name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit (money)
+    @balance += money
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def valid?
+    return true if @status == "open" && @balance > 0
+    false
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
